8d1ed9946d37d2c2cf01f1c7e0fbe2b8073f44bc
Uliana S <uliana.kostiukevych@garantme.fr>
2023-05-02 16:38:24 +0300

    First

---
+++ 1.types.js
@@ -0,0 +1,20 @@
+//Show the value of variables using - console.log(variables)
+//Show the type of value - console.log(typeof(variables))
+console.log("hello");
+
+// 1. Declare integer number and floating-point number variables and show result
+
+
+// 2. Declare string variable and show result
+
+
+// 3. Declare two boolean variable and show result
+
+
+// 4. Declare null variable and show result
+
+
+// 4. Declare undefined variable and show result
+console.log("hello !")
+
+// 6. Declare symbol variable and show result
---
+++ 2.operators.js
@@ -0,0 +1,54 @@
+//Show the value of variables using console.log(variables)
+
+// The following math operators are supported:
+// Addition +,
+// Subtraction -,
+// Multiplication *,
+// Division /,
+// Remainder %,
+// Exponentiation **.
+
+
+// 1. Declare 2 variables, declare a new variable and add first and second variables
+
+
+// 2. Declare 2 variables, declare a new variable and add first and second variables
+
+
+// 3. Declare 2 variables, declare a new variable and subtract the first from the second variable
+
+
+// 4. Declare 2 variables, declare a new variable and multiplicate the first by the second variable
+
+
+// 5. Declare 2 variables, declare a new variable and devide the first by the second variable
+
+
+// 6. Declare 2 variables, declare a new variable and get the division remainder of a number. 
+
+
+// 7. Declare 2 variables, declare a new variable and get the exponent the first by the second variable
+
+
+// Comparison operators :
+// Equal to ==
+// Equal value and equal type ===
+// Not equal !=
+// Not equal value or not equal type !==
+// Greater than >
+// Less than <
+// Greater than or equal to >=
+// Less than or equal to <=
+
+// 8.  Declare 2 variables, declare a new variables and use "==", "===", "!=", "!==" to compare each other
+
+
+// 9.  Declare 2 variables, declare a new variables and use ">", "<", ">=","<=" to compare each other
+
+// Logical operators:
+// Logical AND
+// Logical OR ||
+// Logical NOT !
+
+
+console.log("hello hello world!")
